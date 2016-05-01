18d80785c5a9f74bb665187430bbdef2d4756ea8
Chris <spartanbarr@gmail.com>
2016-05-01 12:58:46 +0930

    Big Overhaul
    Much of the file loading + parsing has been rewritten in C++ instead of blueprint. Significant performance gains, files load and parse > 5x faster.
    
    Extrusion mesh optimised to improve rendering performance. Can now maintain steady framerate on moderately complex models. Extremely complex models are still troublesome, but perform signficantly better than before.
    
    Added ability to roll GCODE forward/back layer by layer, i.e. a print preview.
    
    Overhauled control scheme for KB + Mouse or Xbox Controller + HMD.
    
    Added basic file selection system that is useable in HMD.
    
    Added loading indicator for file parsing to indicate that stuff is happening.
    
    Added ability to render and preview travel moves on printed part.

---
+++ GCODEViewer/Source/GCODEViewer/MyBlueprintFunctionLibrary.h
@@ -0,0 +1,92 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "Kismet/BlueprintFunctionLibrary.h"
+#include "MyBlueprintFunctionLibrary.generated.h"
+
+/**
+ * 
+ */
+
+UENUM(BlueprintType)		//"BlueprintType" is essential to include
+enum class EPrintMoveEnum : uint8
+{
+	PE_Travel 	UMETA(DisplayName = "Travel"),
+	PE_Perimeter 	UMETA(DisplayName = "Perimeter"),
+	PE_Infill	UMETA(DisplayName = "Infill"),
+	PE_Support	UMETA(DisplayName = "Support")
+};
+//
+//USTRUCT()
+//struct PrintMoveStruct
+//{
+//	GENERATED_USTRUCT_BODY()
+//
+//	//Always make USTRUCT variables into UPROPERTY()
+//	//    any non-UPROPERTY() struct vars are not replicated
+//
+//	// So to simplify your life for later debugging, always use UPROPERTY()
+//	UPROPERTY()
+//	FVector EndPoint;
+//
+//	UPROPERTY()
+//	EPrintMoveEnum moveType;
+//
+//}; 
+
+USTRUCT(BlueprintType)
+struct GCODEVIEWER_API FPrintMoveStructCPP
+{
+	GENERATED_USTRUCT_BODY()
+
+	/** Min range value */
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Range)
+	FVector endPoint;
+
+	/** Min range value */
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Range)
+	bool layerChange;
+
+	/** Max range value */
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Range)
+	EPrintMoveEnum moveType;
+
+#if CPP
+	FPrintMoveStructCPP() :
+		endPoint(0.f,0.f,0.f), moveType(EPrintMoveEnum::PE_Travel), layerChange(false)
+	{}
+#endif
+
+
+};
+
+UCLASS()
+class GCODEVIEWER_API UMyBlueprintFunctionLibrary : public UBlueprintFunctionLibrary
+{
+	GENERATED_BODY()
+
+
+public:
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Enum)
+	EPrintMoveEnum PrintMoveEnum;
+
+	UFUNCTION(BlueprintCallable, Category="GCODE")
+	static float getAxisCoordinateCPP(const FString gcodeLine, const FString axisPrefix, bool& foundAxis);
+
+	UFUNCTION(BlueprintCallable, Category = "GCODE")
+	static TArray<FPrintMoveStructCPP> parseGcode(const TArray<FString> gcodeArray, const int prevIndex, const int linesPerTick, FVector& lastPoint, TArray<FPrintMoveStructCPP> moveStruct, int& newIndex, bool& completed, float& percentageProgress);
+	
+	UFUNCTION(BlueprintCallable, Category = "GCODE")
+	static FVector getNewCoordinate(const FString gcodeLine, const FVector lastPoint, bool & success);
+
+	UFUNCTION(BlueprintCallable, BlueprintPure, Category = "GCODE")
+	static FTransform extrudeTransform(const FVector startPoint, const FVector endPoint, const FVector modelCentre);
+
+	//UFUNCTION(BlueprintCallable, BlueprintPure, Category = "GCODE")
+	//static int hmdType();
+	//
+};
+
+
