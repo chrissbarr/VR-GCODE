2d5aea931dcc6146a12c915d00bc8ae8c0d1754a
Chris <spartanbarr@gmail.com>
2016-05-03 22:21:13 +0930

    Vive Controls
    Added some motion control stuff using the Vive controllers. Still haven't mapped basic UI stuff, just object manipulation etc.

--- GCODEViewer/Intermediate/Build/Win64/UE4Editor/Development/GCODEViewer/GCODEViewer.h
+++ GCODEViewer/Intermediate/Build/Win64/UE4Editor/Development/GCODEViewer/GCODEViewer.h
@@ -1,5 +1,5 @@
 #ifndef __AUTO_GCODEVIEWER_H__
 #define __AUTO_GCODEVIEWER_H__
-//Last Write: 4/29/2016 10:08:51 AM +00:00
+//Last Write: 5/1/2016 12:12:52 PM +00:00
 #include "C:\Users\Chris\Documents\Git\VR-GCODE\GCODEViewer\Source\GCODEViewer\GCODEViewer.h"
 #endif//__AUTO_GCODEVIEWER_H__
